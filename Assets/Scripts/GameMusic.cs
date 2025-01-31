7c1bd15d36c7ab0fe8f0bd46e2e8328883b70d1b
Conquistador9 <varuzhan1014@mail.ru>
2025-01-31 13:19:00 +0300

    Commit 7

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour
+{
+    [SerializeField] private string _musicTag;
+
+    private void Awake()
+    {
+        GameObject obj = GameObject.FindWithTag("Music");
+
+        if(obj != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameObject.tag = _musicTag;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
