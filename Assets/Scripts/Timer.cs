da5bc39921cfa48d2012cf1c4eab2cd26b8089de
Conquistador9 <varuzhan1014@mail.ru>
2025-02-01 12:53:08 +0300

    Commit 8

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,17 +5,18 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
     [SerializeField] private InputTouch _inputTouch;
     [SerializeField] private GameObject _gameOverPanel;
     [SerializeField] private TMP_Text _timerText;
-    [SerializeField] private float _maxTImer;
+    [SerializeField] private float _maxTimer;
     [SerializeField] private float _timer;
     [SerializeField] private bool _isTimer= false;
     private GameObject _player;
 
     private void Start()
     {
-        _timer = _maxTImer;
+        _timer = _maxTimer;
         FindPlayer();
     }
 
@@ -26,6 +27,7 @@ public class Timer : MonoBehaviour
             _isTimer = true;
             Debug.Log("lol");
             _timer -= Time.deltaTime;
+            TimerColor();
 
             if(_timer <= 0f)
             {
@@ -58,4 +60,14 @@ public class Timer : MonoBehaviour
     private void FindPlayer() => _player = GameObject.FindGameObjectWithTag("Player");
 
     private void TimerView() => _timerText.text = Mathf.Round(_timer).ToString();
+
+    private void TimerColor()
+    {
+        if (_timer <= 10f)
+        {
+            _timerText.color = Color.red;
+            _animator.enabled = true;
+        }
+        else _timerText.color = Color.green;
+    }
 }
