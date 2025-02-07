7ad0483e008f1e502fe43c3dfb4c99f961cda1d0
Conquistador9 <varuzhan1014@mail.ru>
2025-02-07 15:42:56 +0300

    Commit 11
    CameraMove

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _ball;
+    [SerializeField] private float _positionZ;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position - _ball.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = _ball.position + _offset;
+        newPosition.z = _positionZ;
+        transform.position = newPosition;
+    }
+}
